3ea09ba9800d9ef2211075452123bde6fc6c2a22
MCML.mpi <60952755+MCML-mpi@users.noreply.github.com>
2020-03-04 17:22:24 +0800

    Add files via upload

---
+++ codes/notes - 5/testBackPropXOR.m
@@ -0,0 +1,20 @@
+clear 
+data_input = [ 0, 0, 1; 0, 1, 1; 1, 0, 1; 1, 1, 1]; % training data
+correct_output = [0; 1; 1; 0]; % correct outputs(i.e., labels)
+weight1 = 2 * rand(4, 3) - 1; 
+weight2 = 2 * rand(1, 4) - 1; 
+
+
+for epoch = 1:10000 % train
+[weight1, weight2] = BackPropXOR(weight1, weight2, data_input,...
+, correct_output);
+end
+
+N = 4; % inference
+for k = 1 : N
+x = data_input(k, :)';
+v1 = weight1 * x;
+y1 = sigmoid(v1);
+v = weight2 * y1;
+y = sigmoid(v)
+end
